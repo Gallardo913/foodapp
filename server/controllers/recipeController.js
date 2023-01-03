76bef0ae60b6919473004acd1b83c1f52db07d8b
Roger gallardo <rogergallardofs1@gmail.com>
2023-01-02 19:57:32 -0500

    editing categories page

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,8 +1,69 @@
-
+require('../models/database')
+const Category= require('../models/Category')
 
 
 //Get / homepage
 
 exports.homepage= async(req, res) => {
-    res.render('index', { title:'Rogelios Cooking Blog - Home' });
+
+try{
+
+    const limitNumber= 5;
+    const categories= await Category.find({}).limit(limitNumber);
+
+    res.render('index', { title:'Rogelios Cooking Blog - Home', categories });
+    } catch (error) {
+        res.status(500).send ({message: error.message || "Error Ocurred"});
+    }
 }
+
+//Get / categories
+
+exports.exploreCategories = async(req, res) => {
+
+    try{
+    
+        const limitNumber= 5;
+        const categories= await Category.find({}).limit(limitNumber);
+    
+        res.render('categories', { title:'Rogelios Cooking Blog - Categories', categories });
+        } catch (error) {
+            res.status(500).send ({message: error.message || "Error Ocurred"});
+        }
+    }
+
+
+
+
+
+
+async function insertDymmyCategoryData(){
+    try{
+        await Category.insertMany([
+                  {
+                    "name": "Thai",
+                    "image": "padthai.jpg"
+                  },
+                  {
+                    "name": "American",
+                    "image": "burgernfries.jpg"
+                  }, 
+                  {
+                    "name": "Chinese",
+                    "image": "chineese.jpg"
+                  },
+                  {
+                    "name": "Mexican",
+                    "image": "tacos.jpg"
+                  }, 
+                  {
+                    "name": "Desserts",
+                    "image": "desserts.jpg"
+                  }
+                ]);
+    } catch (error){
+        console.log('err', + error)
+    }
+}
+
+insertDymmyCategoryData();
